ed2838c87e917639e8701e0b513c98adf5f61a97
Kevin Trainor <ktrainor@ligent.net>
2021-02-20 10:22:28 -0600

    Changes for EZU Template assignment partially applied.  Still needs student independent work.

--- courseinfo/views.py
+++ courseinfo/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from courseinfo.models import (
+    Instructor,
+    Section,
+)
+
+
+def instructor_list_view(request):
+    instructor_list = Instructor.objects.all()
+    # instructor_list = Instructor.objects.none()
+    return render(request, 'courseinfo/instructor_list.html', {'instructor_list': instructor_list})
+
+
+def section_list_view(request):
+    section_list = Section.objects.all()
+    # section_list = Section.objects.none()
+    return render(request, 'courseinfo/section_list.html', {'section_list': section_list})
