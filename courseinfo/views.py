ec46fa169aef55f822bbfd5d94a6d80ad58b3ec9
Kevin Trainor <trainor1@illinois.edu>
2021-02-26 14:54:41 -0600

    after template -- synched with ezu_practice.

--- courseinfo/views.py
+++ courseinfo/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-
-from courseinfo.models import (
+from .models import (
     Instructor,
     Section,
+    Course,
+    Semester,
+    Student,
+    Registration,
 )
 
 
@@ -16,3 +19,27 @@ def section_list_view(request):
     section_list = Section.objects.all()
     # section_list = Section.objects.none()
     return render(request, 'courseinfo/section_list.html', {'section_list': section_list})
+
+
+def course_list_view(request):
+    course_list = Course.objects.all()
+    # course_list = Course.objects.none()
+    return render(request, 'courseinfo/course_list.html', {'course_list': course_list})
+
+
+def semester_list_view(request):
+    semester_list = Semester.objects.all()
+    # semester_list = Semester.objects.none()
+    return render(request, 'courseinfo/semester_list.html', {'semester_list': semester_list})
+
+
+def student_list_view(request):
+    student_list = Student.objects.all()
+    # student_list = Student.objects.none()
+    return render(request, 'courseinfo/student_list.html', {'student_list': student_list})
+
+
+def registration_list_view(request):
+    registration_list = Registration.objects.all()
+    # registration_list = Registration.objects.none()
+    return render(request, 'courseinfo/registration_list.html', {'registration_list': registration_list})
