b81f38f8653543327210d6240de10e6d69ec82e3
Kevin Trainor <ktrainor@ligent.net>
2021-04-17 17:26:38 -0500

    code and test deployment coding assignment part 1.

--- trainor_kevin_ezu/settings/base.py
+++ trainor_kevin_ezu/settings/base.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'trainor_kevin_ezu.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / '../db.sqlite3',
     }
 }
 
@@ -122,7 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, '../static')
 
 LOGIN_REDIRECT_URL = 'about_urlpattern'
 
